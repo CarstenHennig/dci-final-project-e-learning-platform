f07eb6d0661791db20622238928f32dad6e6bc2e
CarstenHennig <carsten.hennig@gmail.com>
2021-11-22 14:53:07 +0100

    Added About

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,5 +1,8 @@
 import "./About.css";
 import Carousel from "react-bootstrap/Carousel";
+import Pic1 from "../images/pixelcells3.png";
+import Pic2 from "../images/pixelcells.png";
+import Pic3 from "../images/pixelcells2.png";
 
 function About() {
   return (
@@ -7,58 +10,56 @@ function About() {
       <div className="about_head"></div>
       <div className="about_carousel">
         <Carousel fade>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://pixabay.com/images/id-4986076/"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>#NewDesignThinking</h3>
-              <p>
+          <Carousel.Item className="d-flex justify-content-center">
+            <img className="d-block w-50" src={Pic1} alt="First slide" />
+            <Carousel.Caption className="p-5">
+              <h2>
                 <b>WHY</b>
-              </p>
-              <p>
-                Hybrid learning is one of the main trends in the digitization
-                for each, younger students and working adults. Our low code SaaS
-                solution enables drivers of universities, high schools, schools
-                and even every kind of enterprise or even non-government
-                organizations to setup, design and manage individual platforms
-                for e-learning.
-              </p>
+              </h2>
+              <h4>
+                <b>
+                  Hybrid learning is one of the main trends in the digitization
+                  for each, younger students and working adults. Our low code
+                  SaaS solution enables drivers of universities, high schools,
+                  schools and even every kind of enterprise or even
+                  non-government organizations to setup, design and manage
+                  individual platforms for e-learning.
+                </b>
+              </h4>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Second slide"
-            />
+          <Carousel.Item className="d-flex justify-content-center">
+            <img className="d-block w-50" src={Pic2} alt="Second slide" />
 
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h2>WHO</h2>
+              <h4>
+                The very first try is created by the 'Digital Evangelists'
+                Ferdinand Mbucha, Lead Backend Developer Karin Yahud, Lead
+                Frontend Developer Carsten Hennig, Digital Strategist
+              </h4>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
+          <Carousel.Item className="d-flex justify-content-center">
+            <img className="d-block w-50" src={Pic3} alt="Third slide" />
 
             <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
+              <h2>HOW</h2>
+              <h4>
+                We create a very first 'beta' e-learning platform for students
+                and alumni of DCI, while structuring and preparing all frontend
+                and backend elements for a low code SaaS solution. The core
+                platform for DCI will be created and curated by the 'Digital
+                Evangelists' team for free. It's an open platform, means: We
+                provide API and support for new developed solutions.
+              </h4>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </div>
-      <footer>
-        <h1> Lets Learn Together </h1>
-      </footer>
+      {/* <footer>
+        <h1> Let's Learn Together </h1>
+      </footer> */}
     </div>
   );
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,11 @@
 import { useContext } from "react";
-import {UserContext} from './InfoProvider';
+import { UserContext } from "./InfoProvider";
 import "../App.css";
 
-
 function Home() {
-  const [isLog, setIsLog] = useContext(UserContext)
-  
-  return (<div> 
-    Wlecome {isLog.user.firstName || 'Guest'}
-  </div>)
+  const [isLog, setIsLog] = useContext(UserContext);
+
+  return <div>Welcome {isLog.user.firstName || "Guest"}</div>;
 }
 
 export default Home;
