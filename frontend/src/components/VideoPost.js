f4be2aeacf4d52fd2fe0f5d5cc03a02e017377bd
Ferdinand <ndimbecha@gmail.com>
2021-12-07 01:16:45 +0100

    mediaPlayer second commit

--- frontend/src/components/VideoPost.js
+++ frontend/src/components/VideoPost.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
-import { Row, Col, ListGroup, Card, ListGroupItem, Alert } from "react-bootstrap"
+import { Row, Col, ListGroup, Card, ListGroupItem, Button, Alert } from "react-bootstrap"
 import axios from "axios"
 import moment from "moment"
 
@@ -9,18 +9,16 @@ const baseUrl = "http://localhost:9000/galleries/getClips"
 function YoutubeEmbed() {
 
 	let [videoData, setVideoData] = useState([])
+	const [count, setCount] = useState(0);
 	useEffect(() => {
 		axios.get(baseUrl)
 			.then(response => {
 				const clips = response.data
-				console.log("CLIPS", clips)
 				let gallery = []
 				clips.forEach((video) => {
 					gallery = gallery.concat(video.videos)
-					console.log("GALLERY", gallery)
 				})
 				setVideoData(gallery)
-				console.log(gallery)
 			}
 			)
 			.catch(error => {
@@ -28,10 +26,33 @@ function YoutubeEmbed() {
 			});
 
 	}, []);
-	console.log("VIDEODATA: ", videoData)
+	/* console.log("VIDEODATA: ", videoData) */
 
+	function Counter() {
+		let prev = count - 1
+		return (
+			<div>
+				<p>Playing: {count} </p>
+				<Button onClick={() => setCount(count - 1)}>
+					Play Prev
+				</Button>
+				<Button onClick={() => setCount(count + 1)}>
+					Play Next
+				</Button>
+			</div>
+		);
+	}
+	/* function Playlist() {
+		let list = ""
+		for (let index = 0; index < videoData.length; index++) {
+			const element = videoData[index]
+			
+			console.log("ELEMENT: ", element.title);
 
-	const data = videoData.length ? videoData[3] : null
+		}
+	}
+	Playlist() */
+	const data = videoData.length ? videoData[count] : null
 
 	return (<div>
 		<h1>YOULEARN !</h1>
@@ -68,20 +89,16 @@ function YoutubeEmbed() {
 				<Card style={{ width: '30rem', height: '50rem', backgroundColor: 'skyBlue' }}>
 					<Card.Header>Related Videos</Card.Header>
 					<ListGroup variant="flush">
-						<ListGroup.Item> </ListGroup.Item>
-						<ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-						<ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+						<ListGroup.Item> <Counter /></ListGroup.Item>
+						<ListGroup.Item>{videoData ? videoData.map((item) =>  <li>{item.videoUrl}</li>) : null}</ListGroup.Item>
+						
 					</ListGroup>
 				</Card>
-				{/*  {data.map((item,key)=>
-               { return
-                <tr key={key}>
-                <td>{item.title}</td>
-                <td>{item.videoUrl}</td>
-                <td>{item.desc}</td>
-              </tr> */}
+				
+					
 
-				{/*  })} */}
+
+			
 			</div>
 		</div>
 
