e5b08eacc5584c35644feffd85de9829752175f8
CarstenHennig <carsten.hennig@gmail.com>
2021-12-09 10:08:44 +0100

    Added field summary to Article.jsx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import "bootstrap/dist/css/boostrap.min.css";
+// import "bootstrap/dist/css/boostrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
