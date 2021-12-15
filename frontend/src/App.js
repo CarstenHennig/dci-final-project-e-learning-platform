b607adc1ffbbc4fa4c4911a20fbbab2c99fe8426
Ferdinand <ndimbecha@gmail.com>
2021-12-15 09:51:24 +0100

    check conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,7 @@ import React from "react";
 // import "../node_modules/font-awesome/css/font-awesome.min.css"; // Importing FontAwesome
 import RouterComponents from "./components/Router.jsx";
 import InfoProvider from "./components/InfoProvider.jsx";
-import VideoGallery from "./components/VideoGallery.jsx";
-import MediaPlayer from "./components/MediaPlayer.js";
+
 
 function App() {
   return (
